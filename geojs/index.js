50c5b6056a1bbb00ff29927f8ccaf60e276dccde
Eric Crews <eric@voxel51.com>
2020-02-29 16:08:01 -0500

    Add index files

---
+++ geojs/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require("express");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/healthz", (req, res) => {
+  res.send("ok");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
