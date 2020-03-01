fe194d4378d331fd75b6e8f9116f48087d24cfbb
Eric Crews <eric@voxel51.com>
2020-02-29 20:20:20 -0500

    Add logs for debugging

--- geojs/index.js
+++ geojs/index.js
@@ -10,7 +10,9 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", async (req, res) => {
+  console.log(req);
   let geo_data = await axios.get(`https://get.geojs.io/v1/ip/geo/${req.query.ip}.json`);
+  console.log(geo_data);
   res.send(geo_data);
 });
 
